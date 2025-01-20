c79c75159aaa485d0624098d1cb6707b0a89d798
NORTH89 <nordintata0@gmail.com>
2025-01-20 19:35:04 +0100

    Create values manager to define margin, padding, and size.

---
+++ lib/presentation/values_manager.dart
@@ -0,0 +1,20 @@
+class AppMargin{
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double large = 24.0;
+  static const double extraLarge = 32.0;
+}
+
+class AppPadding{
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double large = 24.0;
+  static const double extraLarge = 32.0;
+}
+
+class AppSize{
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double large = 24.0;
+  static const double extraLarge = 32.0;
+}
