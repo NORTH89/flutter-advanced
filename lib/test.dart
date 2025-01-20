f2bf5650e477dd4913b9c05cdff08097c30452ec
NORTH89 <nordintata0@gmail.com>
2025-01-20 19:20:46 +0100

    Update package import path from `domain/app.dart` to `app/app.dart`.

--- lib/test.dart
+++ lib/test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_advanced/domain/app.dart';
+import 'package:flutter_advanced/app/app.dart';
 
 class Test extends StatelessWidget {
   const Test({super.key});
