4d4a71bc0f81136ebb1f5d645d96ec050270a767
NORTH89 <nordintata0@gmail.com>
2025-01-20 19:34:47 +0100

    Create font manager to define font family, weight, and size.

--- lib/presentation/font_manager.dart
+++ lib/presentation/font_manager.dart
@@ -13,7 +13,6 @@ class FontManager{
 }
 class FontSize{
   static const double small = 12;
-
   static const double medium = 14;
   static const double large = 16;
   static const double extraLarge = 18;
